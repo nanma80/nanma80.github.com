4482d3b20c23efea7e60f82e2c35dcde121e46fe
nanma80 <mananself@gmail.com>
2013-03-10 23:11:36 -0700

    roll back to highlight

--- lollipop/lib/sticker.js
+++ lollipop/lib/sticker.js
@@ -53,23 +53,23 @@ function Sticker(minRadius, maxRadius, minAngle, maxAngle, indexOfLayer, indexOf
     context.lineWidth = (this.maxRadius - this.minRadius) - 2;
 
     // highlighting by lighting
-    // var lighting = (this.highlighted) ? (this.lighting_highlighted) : (this.lighting_original);
-    var lighting = (this.lighting_original);
+    var lighting = (this.highlighted) ? (this.lighting_highlighted) : (this.lighting_original);
+    // var lighting = (this.lighting_original);
     context.strokeStyle = 'hsl('+ this.hue.toString() +','+ this.saturation + ','+ lighting +')';
     context.stroke();
 
     // highlighting by drawing a dot
-    if (this.highlighted) {
-      var avgRadius = (this.minRadius + this.maxRadius)/2;
-      var avgAngle = (this.minAngle + this.maxAngle)/2;
+    // if (this.highlighted) {
+    //   var avgRadius = (this.minRadius + this.maxRadius)/2;
+    //   var avgAngle = (this.minAngle + this.maxAngle)/2;
 
-      context.beginPath();
-      context.arc(
-        canvasCenter.x - avgRadius * Math.cos(avgAngle - dragAngle - Math.PI/2), 
-        canvasCenter.y + avgRadius * Math.sin(avgAngle - dragAngle - Math.PI/2), 
-        2, 0, 2 * Math.PI, false);
-      context.fillStyle = 'black';
-      context.fill();
-    }
+    //   context.beginPath();
+    //   context.arc(
+    //     canvasCenter.x - avgRadius * Math.cos(avgAngle - dragAngle - Math.PI/2), 
+    //     canvasCenter.y + avgRadius * Math.sin(avgAngle - dragAngle - Math.PI/2), 
+    //     2, 0, 2 * Math.PI, false);
+    //   context.fillStyle = 'black';
+    //   context.fill();
+    // }
   }
 }
